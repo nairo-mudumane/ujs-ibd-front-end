906fe2c1d6f5ff1915b2561a3e9afd1a3dd94db0
nairoMudumane <nazirmudumane@gmail.com>
2021-11-30 05:07:41 +0200

    linked logo

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import logoSrc from '../../assets/images/logo.jpg';
 
 export default function Logo() {
-  const StyledLogo = styled.div`
+  const StyledLogo = styled.a`
     max-width: 4rem;
   `;
   return (
-    <StyledLogo>
+    <StyledLogo href="/">
       <img src={logoSrc} alt="" />
     </StyledLogo>
   );
