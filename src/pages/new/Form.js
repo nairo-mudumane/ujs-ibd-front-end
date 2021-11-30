ce86df9be89880c06ced5200f349b0cce3f90267
nairoMudumane <nazirmudumane@gmail.com>
2021-11-30 04:27:10 +0200

    added a signup student page

--- src/pages/new/Form.js
+++ src/pages/new/Form.js
@@ -1,21 +1,37 @@
+import axios from 'axios';
 import React from 'react';
 import { FormContainer } from './FormStyles';
 
 export default function Form() {
   const inputs = [
-    { type: 'text', label: 'Codigo do aluno', id: 'id_student' },
-    { type: 'text', label: 'Nome', id: 'name' },
-    { type: 'text', label: 'Codigo do talento', id: 'id_talent' },
-    { type: 'text', label: 'Curso', id: 'course' },
-    { type: 'email', label: 'Email', id: 'email' },
-    { type: 'text', label: 'Contacto', id: 'contact' },
-    { type: 'text', label: 'Morada', id: 'address' },
-    { type: 'text', label: 'Numero do documento', id: 'numb_document' },
+    {
+      type: 'text',
+      label: 'Codigo do aluno',
+      id: 'id_student',
+      name: 'id_student',
+    },
+    { type: 'text', label: 'Nome', id: 'name', name: 'name' },
+    {
+      type: 'text',
+      label: 'Codigo do talento',
+      id: 'id_talent',
+      name: 'id_talent',
+    },
+    { type: 'text', label: 'Curso', id: 'course', name: 'course' },
+    { type: 'email', label: 'Email', id: 'email', name: 'email' },
+    { type: 'text', label: 'Contacto', id: 'contact', name: 'contact' },
+    { type: 'text', label: 'Morada', id: 'address', name: 'address' },
+    {
+      type: 'text',
+      label: 'Numero do documento',
+      id: 'numb_document',
+      name: 'numb_document',
+    },
   ];
   const selects = [
     {
       label: 'Tipo de documento',
-      value: 'type_document',
+      name: 'type_document',
       options: [
         { optLabel: 'BI', optValue: 'bi' },
         { optLabel: 'passport', optValue: 'passport' },
@@ -23,7 +39,7 @@ export default function Form() {
     },
     {
       label: 'Genero',
-      value: 'genere',
+      name: 'genere',
       options: [
         { optLabel: 'Masculino', optValue: 'masculino' },
         { optLabel: 'Femenino', optValue: 'femenino' },
@@ -31,8 +47,45 @@ export default function Form() {
     },
   ];
 
+  const [inputValues, setInputValues] = React.useState({
+    id_student: '',
+    name: '',
+    id_talent: '',
+    course: '',
+    email: '',
+    contact: '',
+    address: '',
+    numb_document: '',
+  });
+  const [selectValues, setSelectValues] = React.useState({
+    type_document: 'bi',
+    genere: 'masculino',
+  });
+
+  const handleInputChange = ({ target }) =>
+    setInputValues({ ...inputValues, [target.name]: target.value });
+
+  const handleSelectChange = ({ target }) =>
+    setSelectValues({ ...selectValues, [target.name]: target.value });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const url = 'http://localhost:8081/new';
+    const formFields = Object.assign({}, inputValues, selectValues);
+    formFields['password'] = '123a';
+
+    await axios
+      .post(url, formFields)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
-    <FormContainer>
+    <FormContainer onSubmit={handleSubmit}>
       {inputs.map((input) => (
         <div className="form-group mb-3" key={input.id}>
           <label className="form-label" htmlFor={input.id}>
@@ -41,9 +94,11 @@ export default function Form() {
           <input
             type={input.type}
             id={input.id}
+            name={input.name}
             className="form-control"
             required
             autoComplete="off"
+            onChange={handleInputChange}
           />
         </div>
       ))}
@@ -51,9 +106,13 @@ export default function Form() {
       {selects.map((select, index) => (
         <div className="form-label" key={index}>
           <label className="form-label">{select.label}</label>
-          <select className="form-select">
+          <select
+            className="form-select"
+            name={select.name}
+            onChange={handleSelectChange}
+          >
             {select.options.map((opt, index) => (
-              <option value={opt.optValue} key={index}>
+              <option value={opt.optValue} key={index} required>
                 {opt.optLabel}
               </option>
             ))}
